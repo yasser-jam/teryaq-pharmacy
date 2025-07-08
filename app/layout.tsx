3f25e7f9cefa60e98553c0924edb767471029ba8
Yasser-Jamal-Al-Deen <yasserjamalaldeen@gmail.com>
2025-07-08 16:31:14 +0300

    initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "../components/ui/provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,9 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <Provider>
+          { children }
+        </Provider>
       </body>
     </html>
   );
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import styles from './page.module.css';
+import { Button, ChakraProvider } from '@chakra-ui/react';
+
+export default function Home() {
+  return (
+    <main>
+      <Button>Hello</Button>
+    </main>
+  );
+}
